37255777ab70132a44e8731562cbb41c723cd79b
roush55 <ranaabumelha@gmail.com>
2019-12-11 09:01:14 +0300

    hw objects

---
+++ mtba.rb
@@ -0,0 +1,53 @@
+class Subway
+    attr_reader :red, :green, :orange
+
+    def initialize
+        @red = Line.new("red", ["South Station", "Park Street", "Kendall", "Central", "Harvard", "Porter", "Davis", "Alewife"])
+        @green = Line.new("green", ["Government Center", "Park Street", "Boylston", "Arlington", "Copley", "Hynes", "Kenmore"])
+        @orange = Line.new("orange", ["North Station", "Haymarket", "Park Street", "State", "Downtown Crossing", "Chinatown", "Back Bay", "Forest Hills"])
+    end
+
+    def stops_between_stations(start_line, start_station, end_line, end_station)
+        # Convert string to instance variable
+        # https://stackoverflow.com/questions/29410143/convert-string-to-instance-variables
+        start_line = instance_variable_get("@#{start_line.downcase}")
+        end_line = instance_variable_get("@#{end_line.downcase}")
+       start_station=red.stations.index{|station|station.name==start_station}
+       end_station=red.stations.index{|station|station.name==end_station}
+         
+        if start_line == end_line
+            total=(start_station- end_station).abs
+                puts "total stop:#{total}"
+                return total
+        else 
+            puts ""
+           
+        end
+
+    end
+
+end
+    
+class Line
+    attr_reader :stations, :name
+
+    def initialize(name, stations)
+        @name = name
+        @stations = stations.map { |station| Station.new(station)}
+    end
+
+end
+ 
+
+class Station
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
+end
+
+mbta = Subway.new
+mbta.stops_between_stations('Red', 'South Station', 'Red', 'Alewife')
+
