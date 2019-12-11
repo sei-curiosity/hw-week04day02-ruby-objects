016946714545061a8a0ac40a1c3ac31936f2c94b
Modhi Aldughaiyem <modhi199@gmail.com>
2019-12-11 08:52:19 +0300

    solved

--- mbta.rb
+++ mbta.rb
@@ -12,18 +12,18 @@ class Subway
         # https://stackoverflow.com/questions/29410143/convert-string-to-instance-variables
         start_line = instance_variable_get("@#{start_line.downcase}")
        end_line = instance_variable_get("@#{end_line.downcase}")
-       l1=start_line.stations.map{|k| stations.name }
-    l2=end_line.stations.map{|k| stations.name}
+       l1=start_line.stations.map{|k| k.name }
+    l2=end_line.stations.map{|k| k.name}
     # start_station = instance_variable_get("@#{start_line.downcase}")
     # end_station = instance_variable_get("@#{end_line.downcase}")
         
         if (start_station == end_station && start_line == end_line) 
-        p "whatever"
+      
             return 0
     
     elsif start_line == end_line
         # p start_line.@stations.index(start_station)
-        p "second else"
+        # p "second else"
         (l1.index(start_station) - l1.index(end_station)).abs 
         else
             (l1.index(start_station) - l1.index("Park Street")).abs +  (l2.index(end_station) - l2.index("Park Street")).abs
@@ -55,3 +55,5 @@ end
 mbta = Subway.new
 puts mbta.stops_between_stations('red', 'Alewife', 'Red', 'Alewife')
 puts mbta.stops_between_stations('Red', 'Alewife', 'Red', 'Kendall') 
+puts mbta.stops_between_stations('Red', 'South Station', 'Green', 'Kenmore')
+puts mbta.stops_between_stations('Red', 'Alewife', 'Red', 'South Station')
